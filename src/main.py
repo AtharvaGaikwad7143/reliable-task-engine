159b24fb548bd5a5bfdd5af9ca0b4fc622968e36
Atharva <atharvagaikwad2004@gmail.com>
2026-08-26 01:06:10 +0530

    Implementing Alembic for migration

--- src/main.py
+++ src/main.py
@@ -1,17 +1,17 @@
 from fastapi import FastAPI
 from src.api import tasks
-from src.core.database import engine
-from src.db.models import Base
-from contextlib import asynccontextmanager
+# from src.core.database import engine
+# from src.db.models import Base
+# from contextlib import asynccontextmanager
 
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    # Create tables on startup (For Dev Only. We will use Alembic for Prod.)
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
-    yield
+# @asynccontextmanager
+# async def lifespan(app: FastAPI):
+#     # Create tables on startup (For Dev Only. We will use Alembic for Prod.)
+#     async with engine.begin() as conn:
+#         await conn.run_sync(Base.metadata.create_all)
+#     yield
 
-app = FastAPI(title="Reliable Task Engine", lifespan=lifespan)
+app = FastAPI(title="Reliable Task Engine")
 
 app.include_router(tasks.router)
 
